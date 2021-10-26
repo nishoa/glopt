c5741aaf14988874fd331f0a16b00377caca2bd9
Yura <bondprcuk@gmail.com>
2021-10-26 18:02:30 +0300

    reviews section

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$('.reviews__slider').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+    variableWidth: true
+  });
