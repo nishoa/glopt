277d78250d378d159c23248af57ee928792c7199
Yura <bondprcuk@gmail.com>
2021-11-07 02:51:57 +0200

    adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,5 +5,22 @@ $('.reviews__slider').slick({
     infinite: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
-    variableWidth: true
+    variableWidth: true,
+    responsive: [
+      {
+          breakpoint: 768,
+          settings: {
+              arrows: false,
+              slidesToShow: 1,
+              // centerMode: false
+              // variableWidth: false
+          }
+      }
+    ]
   });
+
+$(document).ready(function() {
+  $('.header__hamburger').click(function(event) {
+    $('.header__hamburger,.header__list,.header__logo').toggleClass('active');
+  });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,20 @@ $('.reviews__slider').slick({
     infinite: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
-    variableWidth: true
+    variableWidth: true,
+    responsive: [
+      {
+          breakpoint: 768,
+          settings: {
+              arrows: false,
+              slidesToShow: 1,
+          }
+      }
+    ]
   });
+
+$(document).ready(function() {
+  $('.header__hamburger').click(function(event) {
+    $('.header__hamburger,.header__list,.header__logo').toggleClass('active');
+  });
+});
